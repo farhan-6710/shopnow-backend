bfdb789edda418d71865c61ebbbe87b943cd792f
Farhan-6710 <itz.me.farhan6710@gmail.com>
2026-03-06 07:22:41 +0530

    fix: use environment variable for server port and add types to tsconfig

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { connectDB, disconnectDB } from './config/db';
 dotenv.config();
 connectDB();
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 const server = app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
